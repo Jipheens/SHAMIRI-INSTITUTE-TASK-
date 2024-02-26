dfb0cbb4879ce2bb8219ff61f82246165cdb3683
Teddy Juma <tedd.juma@outlook.com>
2024-02-26 15:42:23 +0300

    Disabled authentication redirect

--- src/layouts/Authenticated.tsx
+++ src/layouts/Authenticated.tsx
@@ -1,21 +1,21 @@
-import React, { ReactNode, useEffect } from 'react';
-import { useState } from 'react';
-import jwt from 'jsonwebtoken';
-import { mdiForwardburger, mdiBackburger, mdiMenu } from '@mdi/js';
-import menuAside from '../menuAside';
-import menuNavBar from '../menuNavBar';
-import BaseIcon from '../components/BaseIcon';
-import NavBar from '../components/NavBar';
-import NavBarItemPlain from '../components/NavBarItemPlain';
-import AsideMenu from '../components/AsideMenu';
-import FooterBar from '../components/FooterBar';
-import { useAppDispatch, useAppSelector } from '../stores/hooks';
-import FormField from '../components/FormField';
-import { Field, Form, Formik } from 'formik';
-import { useRouter } from 'next/router';
-import { findMe, logoutUser } from '../stores/authSlice';
+import React, { ReactNode, useEffect } from "react";
+import { useState } from "react";
+import jwt from "jsonwebtoken";
+import { mdiBackburger, mdiForwardburger, mdiMenu } from "@mdi/js";
+import menuAside from "../menuAside";
+import menuNavBar from "../menuNavBar";
+import BaseIcon from "../components/BaseIcon";
+import NavBar from "../components/NavBar";
+import NavBarItemPlain from "../components/NavBarItemPlain";
+import AsideMenu from "../components/AsideMenu";
+import FooterBar from "../components/FooterBar";
+import { useAppDispatch, useAppSelector } from "../stores/hooks";
+import FormField from "../components/FormField";
+import { Field, Form, Formik } from "formik";
+import { useRouter } from "next/router";
+import { findMe, logoutUser } from "../stores/authSlice";
 
-import { hasPermission } from '../helpers/userPermissions';
+import { hasPermission } from "../helpers/userPermissions";
 
 type Props = {
   children: ReactNode;
@@ -32,13 +32,13 @@ export default function LayoutAuthenticated({
   const router = useRouter();
   const { token, currentUser } = useAppSelector((state) => state.auth);
   let localToken;
-  if (typeof window !== 'undefined') {
+  if (typeof window !== "undefined") {
     // Perform localStorage action
-    localToken = localStorage.getItem('token');
+    localToken = localStorage.getItem("token");
   }
 
   const isTokenValid = () => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (!token) return;
     const date = new Date().getTime() / 1000;
     const data = jwt.decode(token);
@@ -50,14 +50,14 @@ export default function LayoutAuthenticated({
     dispatch(findMe());
     if (!isTokenValid()) {
       dispatch(logoutUser());
-      router.push('/login');
+      // router.push('/login');
     }
   }, [token, localToken]);
 
   useEffect(() => {
     if (!permission || !currentUser) return;
 
-    if (!hasPermission(currentUser, permission)) router.push('/error');
+    if (!hasPermission(currentUser, permission)) router.push("/error");
   }, [currentUser, permission]);
 
   const darkMode = useAppSelector((state) => state.style.darkMode);
@@ -71,59 +71,59 @@ export default function LayoutAuthenticated({
       setIsAsideLgActive(false);
     };
 
-    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on("routeChangeStart", handleRouteChangeStart);
 
     // If the component is unmounted, unsubscribe
     // from the event with the `off` method:
     return () => {
-      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off("routeChangeStart", handleRouteChangeStart);
     };
   }, [router.events, dispatch]);
 
-  const layoutAsidePadding = 'xl:pl-60';
+  const layoutAsidePadding = "xl:pl-60";
 
   return (
     <div
       className={`${
-        darkMode ? 'dark' : ''
+        darkMode ? "dark" : ""
       } overflow-hidden lg:overflow-visible`}
     >
       <div
         className={`${layoutAsidePadding} ${
-          isAsideMobileExpanded ? 'ml-60 lg:ml-0' : ''
+          isAsideMobileExpanded ? "ml-60 lg:ml-0" : ""
         } pt-14 min-h-screen w-screen transition-position lg:w-auto bg-gray-50 dark:bg-slate-800 dark:text-slate-100`}
       >
         <NavBar
           menu={menuNavBar}
           className={`${layoutAsidePadding} ${
-            isAsideMobileExpanded ? 'ml-60 lg:ml-0' : ''
+            isAsideMobileExpanded ? "ml-60 lg:ml-0" : ""
           }`}
         >
           <NavBarItemPlain
-            display='flex lg:hidden'
+            display="flex lg:hidden"
             onClick={() => setIsAsideMobileExpanded(!isAsideMobileExpanded)}
           >
             <BaseIcon
               path={isAsideMobileExpanded ? mdiBackburger : mdiForwardburger}
-              size='24'
+              size="24"
             />
           </NavBarItemPlain>
           <NavBarItemPlain
-            display='hidden lg:flex xl:hidden'
+            display="hidden lg:flex xl:hidden"
             onClick={() => setIsAsideLgActive(true)}
           >
-            <BaseIcon path={mdiMenu} size='24' />
+            <BaseIcon path={mdiMenu} size="24" />
           </NavBarItemPlain>
           <NavBarItemPlain useMargin>
             <Formik
               initialValues={{
-                search: '',
+                search: "",
               }}
               onSubmit={(values) => alert(JSON.stringify(values, null, 2))}
             >
               <Form>
                 <FormField isBorderless isTransparent>
-                  <Field name='search' placeholder='Search' />
+                  <Field name="search" placeholder="Search" />
                 </FormField>
               </Form>
             </Formik>
